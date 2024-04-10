59ff3f6bfc41cc5fc8f875f4d3495ade24041c7f
Someniam <aaronromero489@gmail.com>
2024-04-10 13:49:14 -0700

    add selfie

---
+++ portfolio/selfie1/selfie1.js
@@ -0,0 +1,18 @@
+function setup() {
+createCanvas(600, 600);
+}
+function draw() {
+  background(204);
+  point(300,300);
+
+fill(194, 165, 118)
+ellipse(300, 300, 300, 310);
+arc(160,290,80,80, HALF_PI, PI+HALF_PI);
+arc(440,290,80,80, PI+HALF_PI, HALF_PI);
+triangle(285,320,315,320,300,250)
+fill(255)
+ellipse(240,250,32,20)
+ellipse(360,250,32,20)
+arc(300, 360, 85, 55, 0, PI);
+
+}
